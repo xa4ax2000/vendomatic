a7e1253fec1e4b746a246c5b3c3d5f2744966500
Andrew Hyun <andrew.hyun@alumni.stonybrook.edu>
2020-07-04 09:01:49 -0400

    setting up spring boot starter-web maven project

---
+++ src/main/java/com/codingsample/vendomatic/VendomaticApplication.java
@@ -0,0 +1,13 @@
+package com.codingsample.vendomatic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendomaticApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendomaticApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codingsample/vendomatic/VendomaticApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingsample.vendomatic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendomaticApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
